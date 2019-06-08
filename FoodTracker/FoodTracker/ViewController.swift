2e4491ed53a86522a40bcfef2cb9b6fbafc01f49
Ale <alemoryta@gmail.com>
2019-06-08 19:12:46 -0300

    Creating project
    Creating the foodTracker project on Xcode.

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Alessandro Moryta Suemasu on 08/06/19.
+//  Copyright © 2019 Alessandro Moryta Suemasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
